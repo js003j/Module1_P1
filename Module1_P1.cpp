daccbc008f6fbf21a1bd0c95d9dfc6fd5c3175d2
js003j <js003j@gmail.com>
2025-09-22 00:05:35 -0400

    Add files via upload

---
+++ Module1_P1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Fictional person information
+    string firstName = "James";
+    string lastName = "Sauer";
+    string streetAddress = "754 Washington Blvd";
+    string city = "Aberdeen";
+    string zipCode = "27783";
+
+    // Display the information
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Street Address: " << streetAddress << endl;
+    cout << "City: " << city << endl;
+    cout << "Zip Code: " << zipCode << endl;
+
+    return 0;
+}
