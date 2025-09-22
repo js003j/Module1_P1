e50ff2adec840cf125cd93854b869585384bea3c
js003j <js003j@gmail.com>
2025-09-22 00:07:41 -0400

    Add files via upload

---
+++ Module1_P2.cpp
@@ -0,0 +1,21 @@
+/* Simple Program with a few Errors for Correction
+   Please be sure to correct all outlined errors.
+*/
+
+#include <iostream>
+// #include <conio.h> // Optional: Not used in this corrected program
+
+// Standard namespace declaration
+using namespace std;
+
+// Main Function
+int main()
+{
+    // Standard Output Statement
+    cout << "Welcome to this C++ Program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+
+    // Wait For Output Screen (if needed, system("pause"); could be used on Windows)
+    // Main Function return Statement
+    return 0;
+}
