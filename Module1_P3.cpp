496390560c975bd94b743b8dcd9619f5e2e97fd0
js003j <js003j@gmail.com>
2025-09-22 00:07:54 -0400

    Add files via upload

---
+++ Module1_P3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+// #include <conio.h> // Optional: Only needed on Windows for getch()
+
+// Standard namespace declaration
+using namespace std;
+
+// Main Function
+int main()
+{
+    double myMoney = 1000.50; // This should be printed out
+
+    // Standard Output Statements
+    cout << "Please be sure to correct all syntax errors in this program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+    cout << "The total amount of money available is = " << myMoney << endl;
+
+    // Wait For Output Screen (optional)
+    // system("pause"); // Uncomment this line if you're using Windows
+
+    // Main Function return Statement
+    return 0;
+}
